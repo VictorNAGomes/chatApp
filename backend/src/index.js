96e9ef282f8bccd1ef94b39a088411720752fcce
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-10-01 19:12:38 -0300

    feat: add routes to the index

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,12 @@ const app = express()
 const server = require('http').createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
+const Router = require('./routes'); 
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(Router);
 
 io.on('connection', socket => {
   console.log('User logged in :)')
@@ -10,7 +16,6 @@ io.on('connection', socket => {
     io.emit('chatMessage', msg)
   })
 })
-
 
 
 server.listen(8080, () => console.log('Server running on port 8080'))
